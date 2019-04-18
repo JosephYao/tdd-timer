5766782a477cdf63fe0fdbc13a8e53b25062df6e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-04-18 18:01:39 +0200

    :sparkles: should call callback after one second. fake implementation without considering the actual time passed

--- src/com/odde/timer/Timer.java
+++ src/com/odde/timer/Timer.java
@@ -3,11 +3,15 @@ package com.odde.timer;
 import java.time.Clock;
 
 public class Timer {
-    public Timer(int countDownSecond, Clock clock) {
+    private int countDownSecond;
 
+    public Timer(int countDownSecond, Clock clock) {
+        this.countDownSecond = countDownSecond;
     }
 
     public void start(Runnable runnable) {
-
+        while (countDownSecond-- > 0) {
+            runnable.run();
+        }
     }
 }
